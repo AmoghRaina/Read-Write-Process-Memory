0b9a0dd6e7a07347db39be3bd369d7d16de487a7
AmoghRaina <amoghraina2000@gmail.com>
2024-07-02 21:19:39 +0530

    Title bar changes

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     MainWindow w;
 
-    w.setWindowTitle("Project-RWPM");
+    w.setWindowTitle("Project-RWPM-(By Amogh Raina)");
     w.show();
     w.setWindowIcon(QIcon(":/logo/Resources/file.ico"));
     return app.exec();
